698edebb3587fd66da3c98cbdfbd734eeaa6d109
jack2gs <sailor.g@163.com>
2016-08-06 16:27:16 +0800

    Initial Commit

---
+++ BoundAndFrameSpike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoundAndFrameSpike
+//
+//  Created by Gao Song on 8/6/16.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoundAndFrameSpike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoundAndFrameSpike
+//
+//  Created by Gao Song on 8/6/16.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
